e2dd3b1bbbe00f7a2d7cb8400b77b3b85e99833a
JY <jytan023@gmail.com>
2025-11-29 18:40:30 +0800

    add vision_web_browser.py

--- main.py
+++ main.py
@@ -1,7 +1,70 @@
 from smolagents import LiteLLMModel
+from transformers import AutoTokenizer
+
 
 model = LiteLLMModel(
     model_id = 'ollama_chat/Gemma3:4b',
     api_base = 'http://127.0.0.1:11434',
     num_ctx = 8192,
 )
+
+tokenizer = AutoTokenizer.from_pretrained('ollama_chat/Gemma3:4b')
+rendered_prompt = tokenizer.apply_chat_template(
+    messages, tokenize = False, add_generation_prompt = True
+)
+
+@tool
+def calculator(a: int, b: int) -> int:
+    """Multiple 2 integers together"""
+    """Tool Name: calculator, Description: Multiply two integers., Arguments: a: int, b: int, Outputs: int"""
+    return a* b
+
+print(calculator.to_string())
+
+from typing import Callable
+
+
+class Tool:
+    """
+    A class representing a reusable piece of code (Tool).
+
+    Attributes:
+        name (str): Name of the tool.
+        description (str): A textual description of what the tool does.
+        func (callable): The function this tool wraps.
+        arguments (list): A list of arguments.
+        outputs (str or list): The return type(s) of the wrapped function.
+    """
+    def __init__(self,
+                 name: str,
+                 description: str,
+                 func: Callable,
+                 arguments: list,
+                 outputs: str):
+        self.name = name
+        self.description = description
+        self.func = func
+        self.arguments = arguments
+        self.outputs = outputs
+
+    def to_string(self) -> str:
+        """
+        Return a string representation of the tool,
+        including its name, description, arguments, and outputs.
+        """
+        args_str = ", ".join([
+            f"{arg_name}: {arg_type}" for arg_name, arg_type in self.arguments
+        ])
+
+        return (
+            f"Tool Name: {self.name},"
+            f" Description: {self.description},"
+            f" Arguments: {args_str},"
+            f" Outputs: {self.outputs}"
+        )
+
+    def __call__(self, *args, **kwargs):
+        """
+        Invoke the underlying function (callable) with provided arguments.
+        """
+        return self.func(*args, **kwargs)
